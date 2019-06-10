21cbf2c7a0e438c26a6894d925e63d35adcdfac0
Ilia Kuzmin <i.kyzmin.v@gmail.com>
2019-06-10 22:28:13 +0300

    new search logic

--- kotr-imap/src/main/java/com/example/kotr_imap/ImapExample.kt
+++ kotr-imap/src/main/java/com/example/kotr_imap/ImapExample.kt
@@ -21,37 +21,23 @@ var tagCounter = 0
 @KtorExperimentalAPI
 fun main() {
     runBlocking {
-        val socket = aSocket(ActorSelectorManager(Dispatchers.IO)).tcp().connect(InetSocketAddress("imap.collabio.team", 993)).tls(Dispatchers.IO)
+        val socket =
+            aSocket(ActorSelectorManager(Dispatchers.IO)).tcp().connect(InetSocketAddress("imap.collabio.team", 993))
+                .tls(Dispatchers.IO)
         val w = socket.openWriteChannel(autoFlush = false)
         val r = socket.openReadChannel()
         w.write("$TAG${tagCounter++} LOGIN ilya.kyzmin@myoffice.team 09gw7vkx\r\n")
-        w.flush()
         w.write("$TAG${tagCounter++} LIST \"\" \"*\"\r\n")
-        w.flush()
         w.write("$TAG${tagCounter++} LIST \"\" \"*\" RETURN (STATUS (MESSAGES UNSEEN))\r\n")
-        w.flush()
         w.write("$TAG${tagCounter++} ENABLE QRESYNC\r\n")
-        w.flush()
         w.write("$TAG${tagCounter++} SELECT INBOX (QRESYNC (0 0))\r\n")
+        w.write("$TAG${tagCounter++} UID SORT RETURN (SAVE COUNT) (DATE) UTF-8 OR FROM \"test\" SUBJECT \"test\" \r\n")
+        w.write("$TAG${tagCounter++} UID FETCH $ (UID ENVELOPE) \r\n")
+        w.write("$TAG${tagCounter++} UID SORT RETURN (SAVE) (DATE) UTF-8 NOT UID $ NOT FROM \"test\" NOT SUBJECT \"test\" BODY \"test\" \r\n")
+        w.write("$TAG${tagCounter++} UID FETCH $ (UID ENVELOPE) \r\n")
         w.flush()
-        w.write("$TAG${tagCounter++} SEARCH RETURN (PARTIAL 1:20) ALL \r\n")
-        w.flush()
-       // w.write("$TAG${tagCounter++} UID FETCH \$ (ENVELOPE) \r\n")
-        w.flush()
-        while (true){
-            val line  = r.readUTF8Line().toString()
-            println(line)
-            if (line.contains("PARTIAL")){
-                if(line.indexOf("(")>=0) {
-                    var response = line.substring(line.lastIndexOf("(")+1, line.lastIndexOf(")"))
-                    response = response.split(" ")[1]
-                    w.write("$TAG${tagCounter++} FETCH $response (ENVELOPE) \r\n")
-                    w.flush()
-                    while(r.readUTF8LineTo(System.out)){
-                        println()
-                    }
-                }
-            }
+        while (r.readUTF8LineTo(System.out)) {
+            println()
         }
     }
 }
